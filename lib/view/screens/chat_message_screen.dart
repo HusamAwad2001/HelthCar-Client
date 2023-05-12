2e5422397a0eccad2681aa85f7673590ad30b603
Husam Dahliz <www.hosamawad2016@gmail.com>
2023-05-12 19:12:16 +0300

    Drawer Widget

--- lib/view/screens/chat_message_screen.dart
+++ lib/view/screens/chat_message_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
 import 'package:get/get.dart';
+import 'package:helth_care_client/constants/app_styles.dart';
+import 'package:helth_care_client/constants/constants.dart';
 import 'package:helth_care_client/controllers/chat_controller.dart';
 
 import '../../models/chat_message.dart';
@@ -9,10 +11,20 @@ import '../../services/firestore_helper.dart';
 
 class ChatMessageScreen extends StatelessWidget {
   ChatUser otherUser = Get.arguments;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(otherUser.email!)),
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () => Get.back(),
+          child: const Icon(Icons.arrow_back_ios),
+        ),
+        title: Text(
+          '${otherUser.firstName!} ${otherUser.secondName!} ${otherUser.familyName!}',
+          style: getSemiBoldStyle(),
+        ),
+      ),
       body: GetBuilder<ChatController>(
         builder: (provider) {
           return Column(
@@ -28,57 +40,58 @@ class ChatMessageScreen extends StatelessWidget {
                       return const Center(child: CircularProgressIndicator());
                     } else if (snapshot.hasData) {
                       return ListView.builder(
+                        physics: const BouncingScrollPhysics(),
+                        padding: const EdgeInsets.only(bottom: 10),
                         itemCount: snapshot.data!.length,
                         itemBuilder: (context, index) {
                           return snapshot.data![index].isFromMe
-                              ? ChatBubble(
-                                  clipper: ChatBubbleClipper1(
-                                      type: BubbleType.sendBubble),
-                                  alignment: Alignment.topRight,
-                                  margin: const EdgeInsets.only(top: 20),
-                                  backGroundColor: Colors.blue,
-                                  child: Container(
-                                    constraints: BoxConstraints(
-                                      maxWidth:
-                                          MediaQuery.of(context).size.width *
+                                  ? ChatBubble(
+                                      clipper: ChatBubbleClipper1(
+                                          type: BubbleType.sendBubble),
+                                      alignment: Alignment.topRight,
+                                      margin: const EdgeInsets.only(top: 20),
+                                      backGroundColor: Colors.blue,
+                                      child: Container(
+                                        constraints: BoxConstraints(
+                                          maxWidth: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
                                               0.7,
-                                    ),
-                                    child: Text(
-                                      snapshot.data![index].content,
-                                      style:
-                                          const TextStyle(color: Colors.white),
-                                    ),
-                                  ),
-                                )
-                              : ChatBubble(
-                                  clipper: ChatBubbleClipper1(
-                                      type: BubbleType.receiverBubble),
-                                  backGroundColor: const Color(0xffE7E7ED),
-                                  margin: const EdgeInsets.only(top: 20),
-                                  child: Container(
-                                    constraints: BoxConstraints(
-                                      maxWidth:
-                                          MediaQuery.of(context).size.width *
+                                        ),
+                                        child: Text(
+                                          snapshot.data![index].content,
+                                          style: const TextStyle(
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                    )
+                                  : ChatBubble(
+                                      clipper: ChatBubbleClipper1(
+                                          type: BubbleType.receiverBubble),
+                                      backGroundColor: const Color(0xffE7E7ED),
+                                      margin: const EdgeInsets.only(top: 20),
+                                      child: Container(
+                                        constraints: BoxConstraints(
+                                          maxWidth: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
                                               0.7,
-                                    ),
-                                    child: Text(
-                                      snapshot.data![index].content,
-                                      style:
-                                          const TextStyle(color: Colors.black),
-                                    ),
-                                  ),
-                                );
+                                        ),
+                                        child: Text(
+                                          snapshot.data![index].content,
+                                          style: const TextStyle(
+                                              color: Colors.black),
+                                        ),
+                                      ),
+                                    );
                         },
                       );
                     } else if (snapshot.data == null ||
                         snapshot.data!.isEmpty) {
                       return Center(
                         child: Text(
-                          'ابدأ التواصل الآن',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyLarge!
-                              .copyWith(color: Colors.black12),
+                          'ابدأ المراسلة الآن',
+                          style: getRegularStyle(color: Colors.black),
                         ),
                       );
                     } else {
@@ -119,7 +132,7 @@ class ChatMessageScreen extends StatelessWidget {
       decoration: const BoxDecoration(
         border: Border(
           top: BorderSide(
-            color: Colors.orange,
+            color: primaryColor,
             width: 2,
           ),
         ),
@@ -135,7 +148,7 @@ class ChatMessageScreen extends StatelessWidget {
                   vertical: 10,
                   horizontal: 20,
                 ),
-                hintText: 'Write your message here...',
+                hintText: 'اكتب رسالتك هنا...',
                 border: InputBorder.none,
               ),
             ),
@@ -147,9 +160,9 @@ class ChatMessageScreen extends StatelessWidget {
             },
             icon: const Icon(
               Icons.send,
-              color: Colors.orange,
+              color: primaryColor,
             ),
-          )
+          ),
         ],
       ),
     );
--- lib/view/screens/navigation/patents_navigation_screen.dart
+++ lib/view/screens/navigation/patents_navigation_screen.dart
@@ -1,11 +1,15 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:helth_care_client/constants/constants.dart';
 import 'package:helth_care_client/controllers/auth_controller.dart';
 import 'package:helth_care_client/controllers/navigation_controller.dart';
-import 'package:helth_care_client/routes/routes.dart';
+import 'package:helth_care_client/core/global.dart';
 import 'package:helth_care_client/view/screens/navigation/chat_screen.dart';
 import 'package:helth_care_client/view/screens/navigation/home_screen.dart';
+
+import '../../../constants/app_styles.dart';
+import '../../widgets/button_widget.dart';
 
 class PatentsNavigationScreen extends GetView<NavigationController> {
   const PatentsNavigationScreen({Key? key}) : super(key: key);
@@ -29,12 +33,76 @@ class PatentsNavigationScreen extends GetView<NavigationController> {
                 fontFamily: 'Expo',
               ),
             ),
-            leading: GestureDetector(
-              onTap: () {
-                AuthController().logout();
-              },
-              child: const Icon(Icons.logout),
-            ),
+          ),
+          drawer: Drawer(
+            width: (Get.width / 3) * 2,
+            child: Column(
+                children: [
+                  Container(
+                    height: 200,
+                    color: primaryColor,
+                    alignment: Alignment.center,
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.white,
+                      child: Text(
+                        Global.user['firstName'],
+                        style: const TextStyle(
+                            fontFamily: 'Expo',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.person_pin, size: 30),
+                    title: Text(
+                      '${Global.user['firstName']} ${Global.user['secondName']} ${Global.user['familyName']}',
+                      style: const TextStyle(fontFamily: 'Expo', fontSize: 12),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.email, size: 30),
+                    title: Text(
+                      Global.user['email'],
+                      style: const TextStyle(fontFamily: 'Expo', fontSize: 12),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.phone, size: 30),
+                    title: Text(
+                      Global.user['phone'],
+                      style: const TextStyle(fontFamily: 'Expo', fontSize: 12),
+                    ),
+                  ),
+                  const Spacer(),
+                  ButtonWidget(
+                    width: Get.width,
+                    onPressed: () {
+                      AwesomeDialog(
+                        context: context,
+                        animType: AnimType.scale,
+                        dialogType: DialogType.success,
+                        dialogBackgroundColor: primaryColor,
+                        title: 'تسجيل الخروج',
+                        desc: 'هل أنت متأكد؟',
+                        titleTextStyle: getBoldStyle(color: Colors.white, fontSize: 15),
+                        descTextStyle: getRegularStyle(color: Colors.white, fontSize: 15),
+                        buttonsTextStyle: getRegularStyle(color: Colors.white, fontSize: 15),
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        btnOkOnPress: () => AuthController().logout(),
+                        btnOkText: 'نعم',
+                        btnCancelOnPress: (){},
+                        btnCancelText: 'لا',
+                      ).show();
+                    },
+                    color: Colors.red,
+                    label: 'تسجيل الخروج',
+                  ).paddingSymmetric(horizontal: 25),
+                  const SizedBox(height: 30),
+                ],
+              ),
           ),
           body: IndexedStack(
             index: controller.selectedIndex,
